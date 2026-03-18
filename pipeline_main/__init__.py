e283e6ab35589999cc37f1a218b9e9af6d4e41aa
“ykzhang”112233 <ykzhang@shao.ac.cn>
2026-03-18 15:25:42 +0800

    minor modify

--- pipeline_main/__init__.py
+++ pipeline_main/__init__.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-__author__ = 'GSG & ZYK & AT'
-__version__ = '0.1.0'
-__date__ = '2021-10-01'
+__author__ = 'Yingkang Zhang, Tao A, Shaoguang Guo, Zhijun Xu'
+__version__ = '0.3.0'
+__date__ = '2026-03-01'
 __institute__ = 'Shanghai Astronomical Observatory'
